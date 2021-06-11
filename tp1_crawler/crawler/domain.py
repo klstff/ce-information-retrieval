cdc988dc8e992027b211c44bfbfcdd8b2e0867a2
Kelly Steffany <kellysteffanys@gmail.com>
2021-06-11 17:09:44 -0300

    domain

---
+++ tp1_crawler/crawler/domain.py
@@ -0,0 +1,31 @@
+from datetime import datetime, timedelta
+
+class Domain():
+	def __init__(self,nam_domain,int_time_limit_between_requests):
+		self.time_last_access = datetime(1970,1,1)
+		self.nam_domain = nam_domain
+		self.int_time_limit_seconds = int_time_limit_between_requests
+  
+	@property
+	def time_since_last_access(self):
+		return datetime.now()-self.time_last_access
+
+	def accessed_now(self):
+		self.time_last_access = datetime.now()
+
+	def is_accessible(self):
+		if self.time_last_access == datetime(1970,1,1) or self.time_since_last_access.seconds >= self.int_time_limit_seconds:
+			return True
+		return False
+
+	def __hash__(self):
+		return None
+
+	def __eq__(self, domain):
+		return None
+
+	def __str__(self):
+		return self.nam_domain
+
+	def __repr__(self):
+		return str(self)
